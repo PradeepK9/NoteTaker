01479193577ec5ba600e2284f067dc73fae4d752
Pradeep_Jaiswal <pradeepjaiswalsidd@gmail.com>
2020-12-10 13:12:29 +0530

    helper class added

---
+++ NoteTaker/src/main/java/com/uiet/helper/FactoryProvider.java
@@ -0,0 +1,21 @@
+package com.uiet.helper;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class FactoryProvider {
+	private static SessionFactory factory = null;
+
+	public static SessionFactory getFactory() {
+		if (factory == null) {
+			factory = new Configuration().configure("com/nt/cfgs/hibernate.cfg.xml").buildSessionFactory();
+		}
+		return factory;
+	}
+
+	public void closeFactory() {
+		if (factory.isOpen()) {
+			factory.close();
+		}
+	}
+}
